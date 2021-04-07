3161b5ecb6a416c8b72f8c142ffd6a54d275c6cb
lemansali <79211103+lemansali@users.noreply.github.com>
2021-04-07 23:00:56 +0300

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,13 @@
+function testAction() {   
+     document.querySelector('#largeBtn').setAttribute('modifier', 'cta');
+  
+};
+
+document.addEventListener('init', function(event) {
+
+    ons.ready(function() {
+      // JS код и използване на Cordova API  
+    });
+    
+});
+    
